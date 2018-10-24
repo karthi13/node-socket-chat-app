bc66a514b6277c1b864e51bec5c47c3a77fb0940
karthi13 <dkarthikeyanmailbox@gmail.com>
2018-10-24 21:47:45 +0200

    connect and disconnect event listners

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
 const PORT = process.env.PORT || 3011;
 
 const publicPath = path.join(__dirname + '/../public');
 app.use(express.static(publicPath)); 
 
-app.listen(PORT,( ) => {
+server.listen(PORT,( ) => {
     console.log(`Listining is Port ${PORT}`);
 })
+
+const io = socketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log('listning to event');
+
+    socket.on('disconnect',()=>{
+        console.log('disconnected from event');
+    })
+})
+
+
 
 console.log(publicPath);
